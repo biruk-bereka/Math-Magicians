b75d3ee14d6a4701c4441df4cef1f41d69cc4026
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-14 21:58:46 +0300

    Update file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
+  const symbols = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   return (
-    <div>APP</div>
+    <div>
+      <Calculator symbols={symbols} />
+    </div>
   );
 }
 
