00ddaeafe650c2db09c5089767d03d5bf7592560
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-14 22:48:35 +0300

    Remove symbols array from App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
-  const symbols = ['AC', '+/-', '%', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
   return (
     <div>
-      <Calculator symbols={symbols} />
+      <Calculator />
     </div>
   );
 }
