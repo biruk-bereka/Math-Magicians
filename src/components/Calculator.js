9a3c1bcaad36e457727c2d61ae805e0098d1197a
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-16 12:09:30 +0300

    Fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,18 +5,14 @@ import './styles/Calculator.css';
 
 function Calculator() {
   const symbols = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-  const [state, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-  const handleClick = (e) => {
-    const result = calculate(state, e.target.innerHTML);
-    setState(result);
+  const [value, setValue] = useState(0);
+
+  const handleClick = (ev) => {
+    setValue((prevState) => calculate(prevState, ev.target.innerText));
   };
 
   const display = () => {
-    const { total, next } = state;
+    const { total, next } = value;
     if (!next) return total;
     return next;
   };
