f7bc73a73927afa2f09db21a8a8d078baf0b70a7
biruk-bereka <birukbeletech1212@gmail.com>
2023-02-16 16:38:20 +0300

    Add logic in to calculator and button file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ value }) {
+function Button(props) {
+  const { value, hanldeClick } = props;
   return (
-    <button type="button" className={`button btn-${value}`}>{value}</button>
+    <button type="button" className={`button btn-${value}`} onClick={hanldeClick}>{value}</button>
   );
 }
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
+  hanldeClick: PropTypes.func.isRequired,
 };
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import Button from './Button';
 import './styles/Calculator.css';
 
 function Calculator() {
-  const symbols = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const symbols = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const initialState = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  const [state, setState] = useState(initialState);
+  const hanldeClick = (e) => {
+    const result = calculate(state, e.target.innerText);
+    setState(result);
+  };
+  const display = () => {
+    const { total, next } = state;
+    if (!next) return total;
+    return next;
+  };
+
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">{display() || '0'}</div>
       <div className="buttons">
         {
             symbols.map((symbol) => (
-              <Button key={symbol} value={symbol} />
+              <Button key={symbol} value={symbol} hanldeClick={hanldeClick} />
             ))
         }
       </div>
